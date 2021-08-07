cd9211a506f8f402fd795ab3532d8caca38fc8f4
partha-sharothi <p.sharothi@gmail.com>
2021-08-07 17:13:41 +0600

    views and urls are added

--- blog_proj/blog/urls.py
+++ blog_proj/blog/urls.py
@@ -1,7 +1,15 @@
 
 from django.urls import path
-from .views import home
+from .import views
 
 urlpatterns = [
-    path('',home, name='home'),
+    # path('',views.home, name='home'),
+    path('',views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(),name='about'),
+    path('post/<pk=int:pk>',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.CreatePostView.as_view(), name='post_new'),
+    path('post/<pk=int:pk>/edit/',views,PostUpdateView.as_view(),name='post_edit'),
+    path('post/<pk=int:pk>/remove/', views.PostDeleteView.as_view(),name='Post_remove'),
+    path('drafts/',views.DraftListView.as_view(),name='post_draft_list'),
 ]
+
